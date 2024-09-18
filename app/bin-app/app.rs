379c3e7872cd4085c08c739a865de62d1e41e7a3
neithanmo <neithanmo@gmail.com>
2024-09-18 15:12:26 -0600

    Ensure binary is compile only if ledger feature is enable

--- app/bin-app/app.rs
+++ app/bin-app/app.rs
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *****************************************************************************/
-
+#![cfg(feature = "ledger")]
 #![no_std]
 #![no_main]
 
