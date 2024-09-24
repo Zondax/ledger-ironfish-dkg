2bc7946272d368e3cefa19135152f43ed166e04b
emmanuelm41 <emmanuelm41@gmail.com>
2024-09-24 16:13:16 -0300

    feat: fix handlers

--- app/src/instructions.rs
+++ app/src/instructions.rs
@@ -36,23 +36,25 @@ impl TryFrom<ApduHeader> for Instruction {
     /// [`sample_main`] to have this verification automatically performed by the SDK.
     fn try_from(value: ApduHeader) -> Result<Self, Self::Error> {
         match (value.ins, value.p1, value.p2) {
-            (0, 0, 0) => Ok(Instruction::GetVersion),
-            (16, 0..=1, 0) => Ok(Instruction::DkgGetIdentity {
+            (0x00, 0, 0) => Ok(Instruction::GetVersion),
+            (0x11, 0..=1, 0) => Ok(Instruction::DkgGetIdentity {
                 review: value.p1 == 1,
             }),
-            (17, 0..=2, 0) => Ok(Instruction::DkgRound1 { chunk: value.p1 }),
-            (18, 0..=2, 0) => Ok(Instruction::DkgRound2 { chunk: value.p1 }),
-            (19, 0..=2, 0) => Ok(Instruction::DkgRound3Min { chunk: value.p1 }),
-            (20, 0..=2, 0) => Ok(Instruction::DkgCommitments { chunk: value.p1 }),
-            (21, 0..=2, 0) => Ok(Instruction::DkgSign { chunk: value.p1 }),
-            (22, 0, 0..=3) => Ok(Instruction::DkgGetKeys { key_type: value.p2 }),
-            (24, 0..=2, 0) => Ok(Instruction::DkgGetPublicPackage),
-            (25, 0, 0) => Ok(Instruction::DkgBackupKeys),
-            (26, 0..=2, 0) => Ok(Instruction::DkgRestoreKeys { chunk: value.p1 }),
-            (27, 0..=255, 0) => Ok(Instruction::GetResult { chunk: value.p1 }),
-            (3..=4, _, _) => Err(AppSW::WrongP1P2),
-            (17..=26, _, _) => Err(AppSW::WrongP1P2),
+            (0x12, 0..=2, 0) => Ok(Instruction::DkgRound1 { chunk: value.p1 }),
+            (0x13, 0..=2, 0) => Ok(Instruction::DkgRound2 { chunk: value.p1 }),
+            (0x14, 0..=2, 0) => Ok(Instruction::DkgRound3Min { chunk: value.p1 }),
+            (0x15, 0..=2, 0) => Ok(Instruction::DkgCommitments { chunk: value.p1 }),
+            (0x16, 0..=2, 0) => Ok(Instruction::DkgSign { chunk: value.p1 }),
+            (0x17, 0, 0..=3) => Ok(Instruction::DkgGetKeys { key_type: value.p2 }),
+            (0x18, 0..=2, 0) => Ok(Instruction::DkgGetPublicPackage),
+            (0x19, 0, 0) => Ok(Instruction::DkgBackupKeys),
+            (0x1a, 0..=2, 0) => Ok(Instruction::DkgRestoreKeys { chunk: value.p1 }),
+            (0x1b, 0..=255, 0) => Ok(Instruction::GetResult { chunk: value.p1 }),
             (0x1c, 0..=2, 0) => Ok(Instruction::ReviewTx { chunk: value.p1 }),
+            // Any supported ins with wrong p1 p2 should fall here
+            (0x00, _, _) => Err(AppSW::WrongP1P2),
+            (0x11..=0x1c, _, _) => Err(AppSW::WrongP1P2),
+            // Any other value (unsupported ins) should fall here
             (_, _, _) => Err(AppSW::InsNotSupported),
         }
     }
