da8b50936078ba0007e8554eab4da097d0ef2c1c
emmanuelm41 <emmanuelm41@gmail.com>
2024-09-26 15:02:02 -0300

    feat: move comput_dkg_key to crypto module

--- app/src/crypto.rs
+++ app/src/crypto.rs
@@ -8,6 +8,8 @@ mod utils;
 pub use encryption_keys::*;
 pub use epk::Epk;
 #[cfg(feature = "ledger")]
+pub(crate) use keys::compute_dkg_secret;
+#[cfg(feature = "ledger")]
 pub(crate) use keys::derive_multisig_account;
 pub(crate) use keys::multisig_to_key_type;
 pub use keys::ConstantKey;
