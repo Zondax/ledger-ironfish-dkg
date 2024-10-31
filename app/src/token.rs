465f6b8f5250e61477dde6a7a26f8bbef2467be9
neithanmo <neithanmo@gmail.com>
2024-10-31 04:24:36 -0600

    Add token info resource and handler

---
+++ app/src/token.rs
@@ -0,0 +1,63 @@
+use serde::Deserialize;
+
+use crate::parser::ParserError;
+use crate::token_info::TOKEN_LIST;
+#[derive(Debug, Deserialize)]
+#[serde(bound(deserialize = "'de: 'a"))]
+pub struct TokenList<'a> {
+    #[serde(rename = "schemaVersion")]
+    _schema_version: u32,
+    #[serde(bound(deserialize = "'de: 'a"))]
+    assets: [TokenInfo<'a>; 3],
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(bound(deserialize = "'de: 'a"))]
+pub struct TokenInfo<'a> {
+    #[serde(borrow)]
+    pub identifier: &'a str,
+    #[serde(borrow)]
+    pub symbol: &'a str,
+    pub decimals: u8,
+    #[serde(skip)]
+    _logo_uri: &'a str,
+    #[serde(skip)]
+    _website: &'a str,
+}
+
+pub fn get_token_list() -> Result<TokenList<'static>, ParserError> {
+    let t = serde_json_core::from_str::<TokenList>(TOKEN_LIST)
+        .map(|(t, _)| t)
+        .map_err(|_| ParserError::InvalidTokenList)?;
+    Ok(t)
+}
+
+impl<'a> TokenList<'a> {
+    pub fn get_ticker(&self, asset_id: &str) -> Option<&str> {
+        self.assets.iter().find_map(|asset| {
+            if asset.identifier == asset_id {
+                Some(asset.symbol)
+            } else {
+                None
+            }
+        })
+    }
+
+    pub fn token(&self, asset_id: &str) -> Option<&TokenInfo> {
+        self.assets
+            .iter()
+            .find(|asset| asset.identifier == asset_id)
+    }
+}
+
+#[cfg(test)]
+mod token_list_test {
+    use super::*;
+
+    #[test]
+    fn parse_token_list() {
+        let token_list = get_token_list().unwrap();
+        assert_eq!(token_list.assets.len(), 3);
+        assert_eq!(token_list._schema_version, 2);
+    }
+}
