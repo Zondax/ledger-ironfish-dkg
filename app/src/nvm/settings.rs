a624309eab9a4dfa501fbf93d1ea2779cf51ebd1
emmanuelm41 <emmanuelm41@gmail.com>
2024-11-01 14:28:27 -0300

    feat: add settins nvm storage

---
+++ app/src/nvm/settings.rs
@@ -0,0 +1,35 @@
+use ledger_device_sdk::nvm::*;
+use ledger_device_sdk::NVMData;
+
+// This is necessary to store the object in NVM and not in RAM
+const SETTINGS_SIZE: usize = 10;
+#[link_section = ".nvm_data"]
+static mut DATA: NVMData<AtomicStorage<[u8; SETTINGS_SIZE]>> =
+    NVMData::new(AtomicStorage::new(&[0u8; SETTINGS_SIZE]));
+
+#[derive(Clone, Copy)]
+pub struct Settings;
+
+impl Default for Settings {
+    fn default() -> Self {
+        Settings
+    }
+}
+
+impl Settings {
+    #[inline(never)]
+    pub fn get_mut(&mut self) -> &mut AtomicStorage<[u8; SETTINGS_SIZE]> {
+        return unsafe { DATA.get_mut() };
+    }
+
+    #[inline(never)]
+    pub fn get_ref(&mut self) -> &AtomicStorage<[u8; SETTINGS_SIZE]> {
+        return unsafe { DATA.get_ref() };
+    }
+
+    pub fn get_element(&self, index: usize) -> u8 {
+        let storage = unsafe { DATA.get_ref() };
+        let settings = storage.get_ref();
+        settings[index]
+    }
+}
